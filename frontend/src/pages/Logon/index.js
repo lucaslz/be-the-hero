aa170f26e4816715b1acc7e51fc84b5656163303
Lucas Lima <lucas.developmaster@gmail.com>
2020-03-25 17:36:59 -0300

    Terceiro dia da semana OmniStack finalizando a interface web

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import api from './../../services/api';
+
+import logoImg from './../../assets/logo.svg';
+import heroesImg from './../../assets/heroes.png';
+
+import './style.css';
+
+export default function Logon() {
+
+    const [ id, setId ] = useState('');
+    const history = useHistory();
+
+    async function handleLogon(e) {
+        e.preventDefault();    
+        
+        try {
+            const response = await api.post('sessions', { id });
+
+            if (response) {
+                localStorage.setItem('idOng', id);
+                localStorage.setItem('nameOng', response.data.name);
+                history.push('/profile');
+            }
+        } catch (error) {
+            alert('Não foi possível realizar o login. Tente Novamente.');
+        }
+    }
+
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                
+                <form onSubmit={handleLogon}>
+                    <h1>Faça seu logon</h1>
+
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02004" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    );
+};
